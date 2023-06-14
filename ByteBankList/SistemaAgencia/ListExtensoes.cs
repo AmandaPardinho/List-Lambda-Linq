12e1e96ca2cf9111ed36c794b97292c4d539d2fb
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-14 18:05:23 -0300

    Criação da classe estática para poder gerar um método de extensão

---
+++ ByteBankList/SistemaAgencia/ListExtensoes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankList.SistemaAgencia
+{
+    public static class ListExtensoes
+    {
+        /* método de extensão =>
+         * para gerar um método de extensão é preciso criar um método estático em uma classe estática e definir como primeiro argumento o tipo que queremos extender 
+         * para sinalizar que é um método de extensão usa-se a palavra reservada 'this' do primeiro argumento
+         */
+        public static void AdicionarVarios(this List<int> listaInteiros, params int[] itens)
+        {
+            foreach (int item in itens)
+            {
+                listaInteiros.Add(item);
+            }
+        }
+    }
+}
