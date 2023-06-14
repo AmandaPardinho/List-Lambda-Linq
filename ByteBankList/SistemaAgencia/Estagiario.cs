8c71bcbecedda279bb33e224ac15a664f0c3bff2
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-06-14 18:06:36 -0300

    Alteração em uma diretiva que estava errada

--- ByteBankList/SistemaAgencia/Estagiario.cs
+++ ByteBankList/SistemaAgencia/Estagiario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ByteBankArrays.Modelos.Funcionarios;
+using ByteBankList.Modelos.Funcionarios;
 
 namespace ByteBankList.SistemaAgencia
 {
